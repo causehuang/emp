36462793a282298d17eed1512777c9875ea3869f
弘毅 黃 <ilikeayu@hotmail.com>
2023-07-12 13:09:09 +0800

    1120712

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     path("",views.index,name="index"),
+    path("<int:questions_id>/",views.detail,name="detail"),
+    path("<int:questions_id>/results",views.results,name="results"),
+    path("<int:questions_id>/vote",views.vote,name="vote"),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world .You're at the polls index.")
+def detail(request):
+    return HttpResponse("Hello, world .You're at the polls index.")
+def results(request,questions_id):
+    response="You're looking at the results of question %s"
+    return HttpResponse(response % questions_id)
+def vote(request,questions_id):
+    return HttpResponse("You're voting on question %s" % questions_id)
